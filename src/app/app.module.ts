2599e1fe46e98d72384c990184a899e9719ddf5f
Kevin Van Rooy <kevinvanrooy96@hotmail.com>
2024-02-04 17:24:25 +0100

    add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
+import { VisionComponent } from './vision/vision.component';
+import { PhotographyComponent } from './photography/photography.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    HomeComponent,
+    VisionComponent,
+    PhotographyComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+    
+      {path: "home", component: HomeComponent},
+      {path: "projects", component: ProjectsComponent},
+      {path: "vision", component: VisionComponent},
+      {path: "photography", component: PhotographyComponent},
+      {path: "contact", component: ContactComponent},
+      {path: "**", redirectTo: "home", pathMatch: "full"}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
